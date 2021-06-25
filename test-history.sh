b58bbd48890fe73a37ed280fdecac526a0792f39
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-25 12:13:28 +0200

    test

--- test-history.sh
+++ test-history.sh
@@ -1,78 +1,58 @@
-echo "SHELL=$SHELL"
+if [ -z "$GSH_TMP" ]
+then
+  export GSH_TMP=.
+fi
 
-echo
-echo "======================================================="
-echo
+test_history() {
+  case "$SHELL" in
+    *bash)
 
-# bash --version
-bash -c '
-export GSH_TMP=.
-export GSH_SHELL=bash
-export HISTFILE="history"
-history -r
-export HISTSIZE=50000
-export HISTFILESIZE=50000
-echo "GSH_TMP=$GSH_TMP, HISTFILE=$HISTFILE"
-echo ">>>fc -l"
-fc -l
-touch A B C
-echo ">>>source ./bin/history_start.sh"
-source ./bin/history_start.sh
-echo "GSH_TMP=$GSH_TMP, HISTFILE=$HISTFILE"
-echo ">>>add_cmd ls A"
-add_cmd ls A
-echo ">>>fc -l"
-fc -l
-echo ">>>add_cmd fc -l"
-add_cmd fc -l
-echo ">>>fc -l"
-fc -l
-echo ">>> history"
-history
-#  echo ">>>fc -l"
-#  fc -l
-#  echo ">>> cat \$HISTFILE"
-#  cat "$HISTFILE"
-echo ">>>source ./bin/history_clean.sh"
-source ./bin/history_clean.sh
-echo ">>>fc -l"
-fc -l
-rm -f a b c A B C
-'
+      bash -c '
+      export GSH_SHELL=bash
+      export HISTFILE="$GSH_TMP/history"
+      history -r
+      export HISTSIZE=10
+      export HISTFILESIZE=10
+      fc -ln
+      source ./bin/history_start.sh
+      add_cmd A
+      add_cmd B
+      add_cmd C
+      fc -ln
+      source ./bin/history_clean.sh
+      fc -ln
+      '
+      ;;
 
-# echo
-# echo "======================================================="
-# echo
+    *zsh)
+      zsh -c '
+      export GSH_SHELL=zsh
+      export HISTFILE="$GSH_TMP/history"
+      fc -R
+      export HISTSIZE=10
+      export SAVEHIST=10
+      fc -ln
+      source ./bin/history_start.sh
+      add_cmd A
+      add_cmd B
+      add_cmd C
+      fc -ln
+      source ./bin/history_clean.sh
+      fc -ln
+      '
+      ;;
+  esac
+}
 
-# zsh --version
-# zsh -c '
-# export GSH_TMP=.
-# export GSH_SHELL=zsh
-# export HISTFILE="history"
-# fc -R "$HISTFILE"
-# export HISTSIZE=50000
-# export SAVEHIST=50000
-# echo "GSH_TMP=$GSH_TMP, HISTFILE=$HISTFILE"
-# echo ">>>fc -l"
-# fc -l
-# touch A B C
-# echo ">>>source ./bin/history_start.sh"
-# source ./bin/history_start.sh
-# echo "GSH_TMP=$GSH_TMP, HISTFILE=$HISTFILE"
-# echo ">>>add_cmd ls A"
-# add_cmd ls A
-# echo ">>>add_cmd ls B"
-# add_cmd ls B
-# echo ">>>add_cmd ls C"
-# add_cmd ls C
-# echo ">>>add_cmd fc -l"
-# fc -l
-# echo ">>>source ./bin/history_clean.sh"
-# source ./bin/history_clean.sh
-# echo ">>>fc -l"
-# fc -l
-# rm -f a b c A B C
-# echo ">>> cat \$HISTFILE"
-# cat "$HISTFILE"
-# '
 
+echo a > "$GSH_TMP/history"
+echo b >> "$GSH_TMP/history"
+echo c >> "$GSH_TMP/history"
+
+output=$(test_history | tr -dc "abcABC")
+rm -f "$GSH_TMP/history" "$GSH_TMP/tmp_history"
+if [ "$output" != "abcABCabc" ]
+then
+  echo "PROBLEM"
+  exit 1
+fi
